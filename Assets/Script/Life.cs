8d62785369e51e27d0a12e673aa984a8559f5397
Erin Luo <haolinl4@uci.edu>
2019-02-11 19:32:38 -0800

    ui

---
+++ Assets/Script/Life.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class Life : MonoBehaviour
+{
+
+    TextMeshProUGUI tm;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        tm = this.GetComponent<TextMeshProUGUI>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        tm.text = string.Format("Life: {0:0}", PlayerMovement.life+1);
+    }
+}
---
+++ Assets/Script/Score.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class Score : MonoBehaviour
+{
+    TextMeshProUGUI tm;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        tm = this.GetComponent<TextMeshProUGUI>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        tm.text = string.Format("Score: {0:0}", EnemyMovement.destroy_count);
+    }
+}
