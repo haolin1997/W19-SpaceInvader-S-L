74f228f884864c9f9641e1505e9a0dbe2c3cf0ae
Erin Luo <haolinl4@uci.edu>
2019-02-11 17:18:54 -0800

    ui

---
+++ Assets/Script/LifeScore.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class LifeScore : MonoBehaviour
+{
+    TextMeshProUGUI tm;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        tm = this.GetComponent<TextMeshProUGUI>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        int destory_count = GameObject.Find("Enemy").GetComponent<EnemyMovement>().destory_count;
+        tm.text = string.Format("Score: {0:0}", destory_count );
+    }
+}
